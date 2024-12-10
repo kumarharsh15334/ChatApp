9660bd6f1ba6696ff533c8d4dc352bfbc410fa38
KumarHarsh <kharsh15334@gmial.com>
2024-12-10 12:24:42 +0530

    Initial commit

---
+++ server/routes/AuthRoutes.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import {
+    getUserInfo,
+    login,
+    signup,
+    updateProfile,
+    addProfileImage,
+    removeProfileImage,
+} from "../controllers/AuthController.js";
+import { verifyToken } from "../middlewares/AuthMiddleware.js";
+import multer from "multer";
+
+const authRoute = Router();
+const upload = multer({ dest: "uploads/profiles/" });
+
+authRoute.post("/signup", signup);
+authRoute.post("/login", login);
+authRoute.get('/user-info', verifyToken, getUserInfo);
+authRoute.post('/update-profile', verifyToken, updateProfile);
+authRoute.post(
+    "/add-profile-image",
+    verifyToken,
+    upload.single("profile-image"),
+    addProfileImage
+);
+authRoute.delete("/remove-profile-image", verifyToken, removeProfileImage)
+
+export default authRoute;
