fe8e8e86963c025b4fbf0e166680bf2c10d91b7d
KumarHarsh <kharsh15334@gmail.com>
2025-01-02 01:44:09 +0530

    searching the contacts

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import authRoute from "./routes/AuthRoutes.js";
+import contactsRoutes from "./routes/ContactRoutes.js";
 
 dotenv.config();
 
@@ -24,7 +25,8 @@ app.use("/uploads/profiles", express.static("uploads/profiles"));
 app.use(cookieParser());
 app.use(express.json());
 
-app.use("/api/auth", authRoute)
+app.use("/api/auth", authRoute);
+app.use("/api/contacts", contactsRoutes)
 
 const server = app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
