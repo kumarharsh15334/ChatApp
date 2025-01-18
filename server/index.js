34d686777311a8090399604faab99b6104e89699
KumarHarsh <kharsh15334@gmail.com>
2025-01-18 14:09:33 +0530

    Messenge send in real and show all messages

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import mongoose from "mongoose";
 import authRoute from "./routes/AuthRoutes.js";
 import contactsRoutes from "./routes/ContactRoutes.js";
 import setupSocket from "./socket.js";
+import messagesRoutes from "./routes/MessagsRoutes.js";
 
 dotenv.config();
 
@@ -28,6 +29,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/contacts", contactsRoutes)
+app.use("/api/messages", messagesRoutes);
 
 const server = app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
