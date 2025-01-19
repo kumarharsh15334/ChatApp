ccb2bb38183ec16a51827b70b9380583750d5058
KumarHarsh <kharsh15334@gmail.com>
2025-01-19 18:00:19 +0530

    Showing the contact name

--- client/src/pages/chat/components/contacts-container/index.jsx
+++ client/src/pages/chat/components/contacts-container/index.jsx
@@ -1,8 +1,28 @@
 import { Heading6 } from "lucide-react";
 import ProfileInfo from "./components/profile-info";
 import NewDM from "./components/new-dm";
+import { useEffect } from "react";
+import { apiClient } from "@/lib/api-client";
+import { GET_DM_CONTACT_ROUTES } from "@/utils/constants";
+import { useAppStore } from "@/store";
+import ContactList from "@/components/contact-list";
 
-const ContactsConatiner = () => {
+const ContactsContainer = () => {
+
+  const { setDirectMessagesContacts, directMessagesContacts } = useAppStore();
+
+  useEffect (() => {
+    const getContacts = async () => {
+      const response = await apiClient.get(GET_DM_CONTACT_ROUTES, {
+        withCredentials: true,
+      });
+      if(response.data.contacts){
+        setDirectMessagesContacts(response.data.contacts)
+      }
+    };
+    getContacts();
+  }, []);
+
   return (
     <div className="relative md:w-[35vw] lg:w-[30vw] xl:w-[20vw] bg-[#1b1c24] border-r-2 border-[#2f303b] w-full ">
       <div className="pt-3">
@@ -12,6 +32,9 @@ const ContactsConatiner = () => {
         <div className="flex items-center justify-between pr-10">
           <Title text="Direct Messages" />
           <NewDM />
+        </div>
+        <div className="max-h-[38vh] overflow-y-auto scrollbar-hidden">
+          <ContactList contacts={directMessagesContacts}></ContactList>
         </div>
       </div>
       <div className="my-5">
@@ -24,7 +47,7 @@ const ContactsConatiner = () => {
   );
 };
 
-export default ContactsConatiner;
+export default ContactsContainer;
 
 const Logo = () => {
   return (
